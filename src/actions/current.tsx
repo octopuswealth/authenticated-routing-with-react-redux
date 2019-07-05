octopuswealth/authenticated-routing-with-react-redux

commit 35645fdc58b279596af138cc2e07e6814b2a7d8c

Add check auth on load, to persist auth across sessions

src/actions/current.tsx
@@ -38,3 +38,11 @@ export function logOut() {
     dispatch(unauthenticate());
   };
 }
+
+export function checkAuthentication() {
+  return async (dispatch: Dispatch<AuthenticationAction, {}, any>) => {
+    const authenticated = await window.localStorage.getItem("authenticated");
+
+    authenticated ? dispatch(authenticate()) : dispatch(unauthenticate());
+  };
+}

src/App.tsx
@@ -5,20 +5,19 @@ import { Route, Router } from "react-router-dom";
 import history from "./history";
 import Nav from "./components/Nav";
 import Pages from "./routes/Pages";
-import { logOut } from "./actions/current";
+import { checkAuthentication } from "./actions/current";
 
 interface IProps {
-  logOutConnect: () => void;
+  checkAuthenticationConnect: () => void;
 }
 
-const App = ({ logOutConnect }: IProps) => {
+const App = ({ checkAuthenticationConnect }: IProps) => {
   React.useEffect(() => {
     getAuthState();
   }, []);
 
   const getAuthState = () => {
-    // normally you'd do fancy stuff with your server, this will assume logged out
-    logOutConnect();
+    checkAuthenticationConnect();
   };
 
   return (
@@ -34,7 +33,7 @@ const App = ({ logOutConnect }: IProps) => {
 }
 
 const mapDispatchToProps = {
-  logOutConnect: logOut
+  checkAuthenticationConnect: checkAuthentication
 };
 
 export default connect(

commit 0b15cda4003656b6bb27c13ac7c35e7d2b5bb4a3

Fix bug introduced in 35645fd, parse local storage string to boolean
Local storage requires us to enter JSON, so we need to parse this before
reading it back. Before it was parsing "true" | "false" as true so
always authenticating even if logged out.

src/actions/current.tsx
@@ -41,8 +41,11 @@ export function logOut() {
 
 export function checkAuthentication() {
   return async (dispatch: Dispatch<AuthenticationAction, {}, any>) => {
-    const authenticated = await window.localStorage.getItem("authenticated");
+    const auth = await window.localStorage.getItem("authenticated");
+    const formattedAuth = typeof auth === "string" ?
+      JSON.parse(auth) :
+      null;
 
-    authenticated ? dispatch(authenticate()) : dispatch(unauthenticate());
+    formattedAuth ? dispatch(authenticate()) : dispatch(unauthenticate());
   };
 }